dcf5c8049a9e0c56742daa72a9f3e8059103ed6a
IanRichins <ianrichins@gmail.com>
2020-04-29 13:31:01 -0600

    Initial Commit

---
+++ RandomCocktailApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomCocktailApp
+//
+//  Created by Ian Richins on 4/29/20.
+//  Copyright © 2020 Ian Richins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
